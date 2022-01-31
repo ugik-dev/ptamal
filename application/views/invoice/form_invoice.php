5e7d998647a251f4cdb48e6559975dd3bd70c03c
ugik <ugik.dev@gmail.com>
2022-01-31 14:35:03 +0700

    reload page inv

--- application/views/invoice/form_invoice.php
+++ application/views/invoice/form_invoice.php
@@ -474,11 +474,11 @@
             if (result.isConfirmed == false) {
                 return;
             }
-            // swal.fire({
-            //     title: 'Loading...',
-            //     allowOutsideClick: false
-            // });
-            // swal.showLoading();
+            swal.fire({
+                title: 'Loading...',
+                allowOutsideClick: false
+            });
+            swal.showLoading();
             $.ajax({
                 url: url,
                 'type': 'POST',
@@ -494,8 +494,8 @@
                     //  return;
                     var d = json['data']
 
-                    // swal.fire(swalSuccessConfigure);
-                    // window.location = '<?= base_url() ?>invoice/show/' + d;
+                    swal.fire(swalSuccessConfigure);
+                    window.location = '<?= base_url() ?>invoice/show/' + d;
                 },
                 error: function(e) {}
             });
